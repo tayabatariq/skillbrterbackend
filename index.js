bd813b71e17c369af93b02902d81fb35f67a1423
Bismamalik06 <168851346+tayabatariq@users.noreply.github.com>
2025-07-04 11:43:45 +0500

    ad

--- index.js
+++ index.js
@@ -6,10 +6,6 @@ const authRoutes = require("./routes/auth");
 const userRoutes=require("./routes/userRoutes")
 // backend/index.js or app.js
 
-app.use(cors({
-  origin: "https://skillbarter-beta.vercel.app", // ✅ tumhara frontend domain
-  credentials: true
-}));
 
 dotenv.config();
 
@@ -17,13 +13,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
 
 app.use("/api", authRoutes);
 app.use("/api/users", userRoutes);
+// app.use(cors({
+//   origin: "https://skillbarter-beta.vercel.app", // ✅ tumhara frontend domain
+//   credentials: true
+// }));
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
